f05f7691bd447ba4677d0679c3d56bb290a5a710
b7sj3o <110724603+b7sj3o@users.noreply.github.com>
2024-08-09 18:47:07 +0300

    Update settings.py

--- backend/finance_tracker/settings.py
+++ backend/finance_tracker/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-from dotenv import load_dotenv
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY_DJANGO")
+SECRET_KEY = "django-insecure-7^-k4unu(5&c1s2+r(y%!=+@+66^no=o8nfq$x5wtm)4o*-ksk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
